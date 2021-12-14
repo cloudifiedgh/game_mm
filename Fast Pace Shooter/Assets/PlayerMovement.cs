577ff0030d0e7ce699056fbe41369ff77eeae931
cloudifiedgh <markhofmann888@gmail.com>
2021-12-14 18:06:12 +0100

    Changed Movement
    added sprinting - and new obstacles

--- Fast Pace Shooter/Assets/PlayerMovement.cs
+++ Fast Pace Shooter/Assets/PlayerMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public int jumpnum = 2;
     public CharacterController controller;
     public float speed = 12f;
+    public float sprintspeed = 16f;
+    public float walkspeed = 11f;
     public float jumpHeight = 3f;
 
     public Transform groundCheck;
@@ -16,34 +18,55 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isGrounded);
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
-
-
         }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 WalkSpeed = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(WalkSpeed * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        controller = GetComponent<CharacterController>();
+
+        if (Input.GetButtonDown("Jump") && jumpnum > 1 && jumpnum < 3)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            jumpnum -= 1;
         }
+
+        if (isGrounded)
+        {
+            jumpnum = 2;
+            controller.stepOffset = 3.8f;
+        }else
+        {
+            controller.stepOffset = 0.0f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && z == 1)
+        {
+            speed = sprintspeed;
+        }else
+        {
+            speed = walkspeed;
+        }
+
+
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
     }
-
 
 }
